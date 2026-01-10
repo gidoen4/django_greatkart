7d501b06e952a66208c96f31eb404c6c6e4fc02a
gideon <gidoenparajuli55@gmail.com>
2026-01-10 19:45:33 +0545

    2nd

--- greatkart/accounts/models.py
+++ greatkart/accounts/models.py
@@ -16,6 +16,21 @@ class MyAccountManager(BaseUserManager):
             last_name = last_name,
 
         )
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+    
+    def create_superuser(self,first_name,last_name,email,username,password):
+        user = self.create_user(
+            email= self.normalize_email(email),
+            username= username,
+            password=password,
+            first_name= first_name,
+            last_name= last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
 
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)

309ce13f2bdf35dd99845c671dfc28052b0e5491
gideon <gidoenparajuli55@gmail.com>
2026-01-10 22:57:38 +0545

    5th

--- greatkart/accounts/admin.py
+++ greatkart/accounts/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
 # Register your models here.
+class AccountAdmin(UserAdmin):
+    list_display = ('email','first_name','last_name','username','last_login','date_joined','is_active')
 
-admin.site.register(Account)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account,AccountAdmin)

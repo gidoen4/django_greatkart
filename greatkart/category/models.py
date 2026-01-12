37ee3e4d575cd82877fa5a9156065a1473eaa78f
gideon <gidoenparajuli55@gmail.com>
2026-01-12 18:54:51 +0545

    1st

--- greatkart/category/models.py
+++ greatkart/category/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class category(models.Model):
+class Category(models.Model):
     category_name = models.CharField(max_length=50,unique=True)
     slug = models.SlugField(max_length=100,unique=True)
     description = models.TextField(max_length=255,blank=True)

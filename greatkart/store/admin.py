595c8217e8abb8f049a9115acca845d462238542
gideon <gidoenparajuli55@gmail.com>
2026-01-12 20:09:31 +0545

    3rd

--- greatkart/store/admin.py
+++ greatkart/store/admin.py
@@ -5,4 +5,4 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name','price','stock','category','modified_date','is_available')
     prepopulated_fields = {'slug':('product_name',)}
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)

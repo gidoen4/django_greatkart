0860f20133bee19f6f476704aa36314c87dc5e44
gideon <gidoenparajuli55@gmail.com>
2026-01-19 23:08:35 +0545

    4th

--- greatkart/carts/views.py
+++ greatkart/carts/views.py
@@ -31,12 +31,29 @@ def add_cart(request,product_id):
             quantity = 1,
             cart = cart,
         )
-        cart_item.save()
-    return HttpResponse(cart_item.product)
-    exit()    
+        cart_item.save()  
     return redirect('cart')    
 
 
-def cart(request):
+def cart(request,total =0,quantity=0,cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,is_active = True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax    
+    except ObjectNotExist:
+        pass # just ignore
 
-    return render(request,'store/cart.html')
+    context = {
+        'total' : total,
+        'quantity' : quantity,
+        'cart_items' : cart_items,
+        'tax' : tax,
+        'grand_total': grand_total,
+    }        
+
+
+    return render(request,'store/cart.html',context)

aed37c532f362dd3a68a5c746e361d6fdafa7e5a
gideon <gidoenparajuli55@gmail.com>
2026-01-19 14:23:17 +0545

    1st

--- greatkart/carts/views.py
+++ greatkart/carts/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from carts.models import Cart
+from store.models import Product
 
+# Create your views here.
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart    
+def add_cart(request,product_id):
+    product = Product.objects.get(id= product_id) #get the product
+
+    try:
+        cart = Cart.objects.get(cart_id =_cart_id(request)) #get the cart using the cart id present in the session
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+    cart.save()       
 def cart(request):
     return render(request,'store/cart.html')

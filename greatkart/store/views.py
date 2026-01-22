02ea5fb994f2cd5a153ba2c1c78f08d110b3142d
gideon <gidoenparajuli55@gmail.com>
2026-01-22 22:38:35 +0545

    2nd

--- greatkart/store/views.py
+++ greatkart/store/views.py
@@ -19,7 +19,7 @@ def store(request,category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available = True)
-        paginator = Paginator(products,6)
+        paginator = Paginator(products,3)
         page =  request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()

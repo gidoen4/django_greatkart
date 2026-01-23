03504a73fed45ef6655a802c2add99ccb41f063f
gideon <gidoenparajuli55@gmail.com>
2026-01-23 19:48:00 +0545

    2nd

--- greatkart/carts/views.py
+++ greatkart/carts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 from carts.models import Cart, CartItem
 from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def _cart_id(request):
@@ -57,6 +58,8 @@ def remove_cart_item(request,product_id):
 
 def cart(request,total =0,quantity=0,cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id = _cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart,is_active = True)
         for cart_item in cart_items:
@@ -64,7 +67,7 @@ def cart(request,total =0,quantity=0,cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax    
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass # just ignore
 
     context = {

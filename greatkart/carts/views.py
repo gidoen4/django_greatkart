6140317a6201847eac66398a2ae20ce146e8ff2a
gideon <gidoenparajuli55@gmail.com>
2026-01-24 22:55:44 +0545

    4th

--- greatkart/carts/views.py
+++ greatkart/carts/views.py
@@ -12,6 +12,10 @@ def _cart_id(request):
         cart = request.session.create()
     return cart    
 def add_cart(request,product_id):
+    color = request.GET['color']
+    size = request.GET['size']
+    return HttpResponse(color + ' ' + size)
+    exit()
     product = Product.objects.get(id= product_id) #get the product
 
     try:

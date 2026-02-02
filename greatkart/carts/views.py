9a0c083933632f3509255d3300a8c276c8b69c2f
gideon <gidoenparajuli55@gmail.com>
2026-02-02 22:06:01 +0545

    3rd

--- greatkart/carts/views.py
+++ greatkart/carts/views.py
@@ -37,6 +37,7 @@ def add_cart(request,product_id):
     try:
         cart_item =  CartItem.objects.get(product=product,cart=cart)
         if len(product_variation) > 0:
+            cart_item.variations.clear()
             for item in product_variation:
                 cart_item.variations.add(item)
         cart_item.quantity += 1 #cart_item.quantity = cart_item.quantity + 1

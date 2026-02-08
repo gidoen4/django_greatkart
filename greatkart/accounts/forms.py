e58c99f5883abe81d98a5389e5a8b57eff2c301b
gideon <gidoenparajuli55@gmail.com>
2026-02-08 19:31:58 +0545

    2nd

--- greatkart/accounts/forms.py
+++ greatkart/accounts/forms.py
@@ -3,9 +3,18 @@ from .models import Account
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder':'Enter Password'
+        'placeholder':'Enter Password',
+        'class' : 'form-control',
     }))
-    
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'confirm Password',
+        'class' : 'form-control',
+    }))
     class Meta:
         model = Account
         fields = ['first_name','last_name','phone_number','email','password']
+
+    def __init__(self,*args,**kwargs):
+        super(RegistrationForm,self).__init__(*args,**kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'    

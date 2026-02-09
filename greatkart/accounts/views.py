af5720c2912b37bf419c966b600e29d617979391
gideon <gidoenparajuli55@gmail.com>
2026-02-09 20:30:31 +0545

    3

--- greatkart/accounts/views.py
+++ greatkart/accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from accounts.models import Account
 from .forms import RegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,6 +21,8 @@ def register(request):
                                               email=email,username = username,password=password)
            user.phone_number = phone_number
            user.save()
+           messages.success(request,'Registration Successful')
+           return redirect('register')
     else:
         form = RegistrationForm()
     context = {

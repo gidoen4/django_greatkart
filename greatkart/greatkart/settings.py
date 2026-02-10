aa4bcc377eb0ed959951e2136e01bdf2dd4c92bc
gideon <gidoenparajuli55@gmail.com>
2026-02-10 20:08:58 +0545

    5

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -137,3 +137,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+#SMTP CONFIGURATION
+
+EMAIL_HOST = 'smtp.gmail.com',
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'victor@gmail.com'
+EMAIL_HOST_PASSWORD = 'victor'
+EMAIL_USE_TLS = True

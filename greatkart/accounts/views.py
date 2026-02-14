0fc9c4e544b61c03935315ffbfc77a388dbd379a
gideon <gidoenparajuli55@gmail.com>
2026-02-14 19:46:39 +0545

    2

--- greatkart/accounts/views.py
+++ greatkart/accounts/views.py
@@ -98,6 +98,28 @@ def dashboard(request):
    return render(request,'accounts/dashboard.html')
 
 def forgotpassword(request):
+    if request.method == 'POST':
+        email = request.method.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+            #user activation
+            current_site = get_current_site(request)
+            mail_subject = 'reset your password'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user':user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+
+            })  
+            to_email = email
+            send_email = EmailMessage(mail_subject,message,to =[to_email])
+            send_email.send()
+
+
+
+        else:
+            messages.error(request,'Account does not exist')    
     return render(request,'account/forgotpassword.html')
 
 

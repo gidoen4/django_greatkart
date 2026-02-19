f2986111befcbadcd693de3d1b35d6083cb7aab3
gideon <gidoenparajuli55@gmail.com>
2026-02-19 23:29:48 +0545

    22

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -142,7 +142,7 @@ from decouple import config
 
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 

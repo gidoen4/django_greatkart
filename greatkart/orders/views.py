464132491c889a817cb8727b076576119a275763
gideon <gidoenparajuli55@gmail.com>
2026-02-21 19:54:01 +0545

    2\

--- greatkart/orders/views.py
+++ greatkart/orders/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 from carts.models import CartItem
+from orders.forms import OrderForm
 
 # Create your views here.
 def place_order(request):
@@ -9,3 +10,9 @@ def place_order(request):
 
     # if the carts count is less than or equal to 0 , thhen redirect them to store
     cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+    
+    if request.method == 'POST':
+        form = OrderForm(request.POST)

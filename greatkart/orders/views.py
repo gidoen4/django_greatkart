d355e60527c4f664933fce490e0ce5453956b873
gideon <gidoenparajuli55@gmail.com>
2026-02-21 20:26:24 +0545

    4

--- greatkart/orders/views.py
+++ greatkart/orders/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
@@ -6,7 +8,7 @@ from .models import Order
 from .forms import OrderForm
 
 # Create your views here.
-def place_order(request):
+def place_order(request,total =0,quantity=0):
     current_user = request.user
 
     # if the carts count is less than or equal to 0 , thhen redirect them to store
@@ -14,6 +16,14 @@ def place_order(request):
     cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('store')
+    
+    grand_total = 0
+    tax = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total)/100
+    grand_total = total + tax    
     
     if request.method == 'POST':
         form = OrderForm(request.POST)
@@ -30,4 +40,23 @@ def place_order(request):
             data.state = form.cleaned_data('state')
             data.city = form.cleaned_data('city')
             data.order_note = form.cleaned_data('order_note')
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+
+            # generate order number
+
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr,mt,dt)
+            current_date = d.strftime("%Y%m%d")
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+            return redirect('checkout')
+
+            
+
           

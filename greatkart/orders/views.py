1a916887c9c077a8b17af4f3751d5ff5bf24a8b2
gideon <gidoenparajuli55@gmail.com>
2026-02-21 19:47:48 +0545

    1

--- greatkart/orders/views.py
+++ greatkart/orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from carts.models import CartItem
 
 # Create your views here.
+def place_order(request):
+    current_user = request.user
+
+    # if the carts count is less than or equal to 0 , thhen redirect them to store
+    cart_items = CartItem.objects.filter(user=current_user)

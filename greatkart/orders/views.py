aa27ee9c45b9281cc4b06943cd1fc48a853d1960
gideon <gidoenparajuli55@gmail.com>
2026-02-27 20:50:18 +0545

    3

--- greatkart/orders/views.py
+++ greatkart/orders/views.py
@@ -142,7 +142,20 @@ def place_order(request,total =0,quantity=0):
         return redirect('checkout')  
 
 def order_complete(request):
-    return render(request,'orders/order_complete.html')      
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number,is_ordered=True)
+        ordered_products = OrderProduct.objects.filterr(order_id=order.id)
+
+        context = {
+            'order' : order,
+            'ordered_products' : ordered_products,
+        }
+        return render(request,'orders/order_complete.html',context)
+    except(Payment.DoesNotExist,Order.DoesNotExist):
+        return redirect('home')     
 
             
 

79464ce8c93fd74ba9061a900de24a043868eaf0
gideon <gidoenparajuli55@gmail.com>
2026-03-01 19:52:07 +0545

    2

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    'greatkart/static',
+    BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 #media files configuration
 
 MEDIA_URL = '/media/'

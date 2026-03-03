83a324d01caedd88ef918fa672c43009f6b58388
gideon <gidoenparajuli55@gmail.com>
2026-03-03 12:52:43 +0545

    1

--- greatkart/store/models.py
+++ greatkart/store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from category.models import Category
 from accounts.models import Account
+from django.db.models import Avg
 
 # Create your models here.
 class Product(models.Model):
@@ -21,6 +22,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+     
+    def averageReview(self):
+         reviews = ReviewRating.objects.filter(product = self,status = True).aggregate(average=Avg('rating'))
+         avg = 0
+         if reviews['average'] is not None:
+              avg = float(reviews['average'])
+         return avg    
 
 class VariationManager(models.Manager):
      def colors(self):
@@ -56,7 +64,7 @@ class ReviewRating(models.Model):
      ip = models.CharField(max_length=20,blank = True)
      status = models.BooleanField(default=True)
      created_date = models.DateTimeField(auto_now_add=True)
-     updated_date = models.DateTimeField(auto_now=True)
+     updated_date = models.DateTimeField(auto_now_add=True)
 
      def __str__(self):
           return self.subject

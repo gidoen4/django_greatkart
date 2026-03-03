f7dbbe112fa9c68b106f5e2ab37241a13c1ce8ad
gideon <gidoenparajuli55@gmail.com>
2026-03-03 13:48:04 +0545

    6

--- greatkart/accounts/views.py
+++ greatkart/accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 import requests
 
 from accounts.models import Account
+from orders.models import Order
 
 from .forms import RegistrationForm
 from django.contrib import messages,auth
@@ -147,7 +148,12 @@ def activate(request,uidb64,token):
 
 @login_required(login_url='login')
 def dashboard(request):
-   return render(request,'accounts/dashboard.html')
+   orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id,is_ordered =True)
+   orders_count = orders.count()
+   context = {
+       'orders_count' :orders_count,
+   }
+   return render(request,'accounts/dashboard.html',context)
 
 def forgotPassword(request):
     if request.method == 'POST':
@@ -211,7 +217,12 @@ def resetPassword(request):
     else:        
       return render(request,'accounts/resetPassword.html')    
 
-
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user,is_ordered = True).order_by('-created_at')
+    context = {
+        'orders' : orders,
+    }
+    return render(request,'accounts/my_orders.html',context)
 
 
 

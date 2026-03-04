f87e2b219a3893a37c91d618de31a25118679fea
gideon <gidoenparajuli55@gmail.com>
2026-03-04 11:37:08 +0545

    1

--- greatkart/accounts/views.py
+++ greatkart/accounts/views.py
@@ -224,6 +224,7 @@ def my_orders(request):
     }
     return render(request,'accounts/my_orders.html',context)
 
-
+def edit_profile(request):
+    return render(request,'accounts/edit_profile.html')
 
 

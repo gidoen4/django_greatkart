665f6138d43d276aa80b390c6a703ac7f151eb68
gideon <gidoenparajuli55@gmail.com>
2026-03-04 11:44:02 +0545

    2

--- greatkart/accounts/forms.py
+++ greatkart/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from .models import Account
+from .models import Account,UserProfile
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
@@ -33,4 +33,14 @@ class RegistrationForm(forms.ModelForm):
         for field in self.fields:
             self.fields[field].widget.attrs['class']='form-control'
 
-              
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('first_name','last_name','phone_number')
+
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('address_line_1','address_line_2','city','state','country','profile_picture')

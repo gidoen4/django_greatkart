b1da82eb95982a158f5f375647e472b4a4d48579
gideon <gidoenparajuli55@gmail.com>
2026-03-07 22:09:40 +0545

    3

--- greatkart/store/admin.py
+++ greatkart/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product,Variation,ReviewRating
+from .models import Product,Variation,ReviewRating,ProductGallery
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -14,3 +14,4 @@ class VariationAdmin(admin.ModelAdmin):
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Variation,VariationAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
--- greatkart/store/models.py
+++ greatkart/store/models.py
@@ -84,4 +84,7 @@ class ProductGallery(models.Model):
      def __str__(self):
           return self.product.product_name
      
+     class Meta:
+          verbose_name = 'productgallery'
+          verbose_name_plural = 'product gallery'
 
